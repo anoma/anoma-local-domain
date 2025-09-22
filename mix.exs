1dac06caee6118ca63e28c71af0518cae5e7e524
mariari <mariari@protonmail.com>
2025-09-22 21:29:14 +0800

    Local Domain V0.2.0 Release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Anoma.LocalDomain.MixProject do
   def project do
     [
       app: :anoma_local_domain,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
