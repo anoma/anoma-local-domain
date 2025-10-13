7e2c04bd379444362b7bbe22ca7816c30bc628d1
agureev <artguril@proton.me>
2025-10-13 15:26:25 +0400

    Optimize Merkle Tree Leaves Addition
    Now merkle tree accepts a list of leaves to add

--- test/merkle_test.exs
+++ test/merkle_test.exs
@@ -3,10 +3,10 @@ defmodule MerkleTest do
   doctest Anoma.LocalDomain.MerkleTree
 
   test "Run the examples" do
-    Examples.EMerkleTree.add_leaf_to_merkle_tree
-    Examples.EMerkleTree.write_to_merkle_tree
-    Examples.EMerkleTree.expand_merkle_tree
-    Examples.EMerkleTree.generate_a_proof
-    Examples.EMerkleTree.verify_a_proof    
+    Examples.EMerkleTree.write_to_merkle_tree()
+    Examples.EMerkleTree.expand_merkle_tree()
+    Examples.EMerkleTree.generate_a_proof()
+    Examples.EMerkleTree.verify_a_proof()
+    Examples.EMerkleTree.random_tree()
   end
 end
