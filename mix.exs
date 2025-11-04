b709553bccde007fa7e2ad61529cc60e9456ddc1
Jam <jwalkerdine21@hotmail.com>
2025-11-04 12:26:51 +0000

    Release V0.3.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Anoma.LocalDomain.MixProject do
   def project do
     [
       app: :anoma_local_domain,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
