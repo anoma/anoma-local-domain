82174dc735463e8e380b469fc69ca152b2653501
Murisi Tarusenga <murisit@gmail.com>
2025-11-21 09:06:01 +0200

    Make transpile take a list of expressions. Always cast identifiers before calling them.

--- lib/examples/e_transpile.ex
+++ lib/examples/e_transpile.ex
@@ -1,26 +1,35 @@
 defmodule Examples.ETranspile do
   require ExUnit.Assertions
-  import ExUnit.Assertions
 
   alias Anoma.LocalDomain.Transpile
 
   def transpile_factorial() do
     state = Transpile.new()
     {state, block} = Transpile.transpile(state,
-      [:function, :main, [],
+      [[:function, :scm_main, [],
        [:function, :factorial, [:n],
         [:if, [:==, :n, 0],
          1,
          [:*, :n, [:factorial, [:-, :n, 1]]]]],
-       [:factorial, 5]],
-      {{:literal_expr, 120}, {:type_name, "uintptr_t", {:identifier_declarator, ""}}})
-    {:expr_stmt,
-     {:binary_expr, binary_op, {:literal_expr, expected_result},
-      {:cast_expr, {:type_name, result_type, _},
-       _}}} = Enum.at(block, -1)
-    assert binary_op == "="
-    assert expected_result == 120
-    assert result_type == "uintptr_t" 
-    {state, block}
+        [:factorial, 5]]])
+    {state, block, Transpile.program_to_string(block)}
+  end
+
+  # let input = Sexpr::from((0..100).map(|x| Sexpr::from(2 * x + 1)).collect::<Vec::<_>>());
+  # let output: Sexpr = receipt.journal.decode().unwrap();
+  def transpile_filter() do
+    state = Transpile.new()
+    {state, block} = Transpile.transpile(state,
+      [[:function, :filter, [:xs, :f],
+        [:if, [:is_null, :xs],
+          [:null],
+          [:if, [:f, [:car, :xs]],
+            [:cons, [:car, :xs], [:filter, [:cdr, :xs], :f]],
+            [:filter, [:cdr, :xs], :f]]]],
+      [:function, :scm_main, [],
+        [[:function, :_, [:n],
+          [:commit_sexpr, [:filter, [:read_sexpr],
+            [:function, :divisible_by_n, [:x], [:==, [:%, [:as_integer, :x], :n], 0]]]]], 3]]])
+    {state, block, Transpile.program_to_string(block)}
   end
 end
